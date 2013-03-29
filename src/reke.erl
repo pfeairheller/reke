1e40cc80952e9bfbeed92e1d7d597899a6b6d385
Philp Feairheller <phil@hoopla.net>
2013-03-28 22:41:53 -0400

    Playing around with migration script creation.  Will need a different module.

--- src/reke.erl
+++ src/reke.erl
@@ -3,13 +3,14 @@
 -author("pfeairheller").
 
 %% API
--export([start/0, migrate/0, rollback/0, generate/0]).
+-export([start/0, execute/0]).
+-export([migrate/1, rollback/1, generate/1]).
 
 
 start() ->
   reke_connection:start(reke).
 
-generate() ->
+execute() ->
   receive
     {create_table, TableName, Contents} ->
       Stmt = generate_create_statement(TableName, Contents),
@@ -19,14 +20,14 @@ generate() ->
         Result ->
           erlang:display(Result)
       end,
-      generate();
+      execute();
     {drop_table, TableName} ->
       Stmt = generate_drop_statement(TableName),
       case reke_connection:squery(reke, Stmt) of
         Result ->
           erlang:display(Result)
       end,
-      generate();
+      execute();
     {finished, Pid} ->
       Pid ! done,
       ok;
@@ -34,15 +35,23 @@ generate() ->
       {error, unknown_command}
   end.
 
-migrate() ->
+migrate(_Options) ->
   run_for(up).
 
-rollback() ->
+rollback(_Options) ->
   run_for(down).
 
+generate(["model" | [ModelName | ColumnSpecs]]) ->
+  io:format("Generating model ~s with columns ~p~n", [ModelName, ColumnSpecs]),
+  {{Year, Month, Day}, {Hour, Min, Sec}} = calendar:local_time(),
+  FileName = io_lib:format("db/migrate/~p~2..0B~2..0B~2..0B~2..0B~2..0B_create_~s.ebm", [Year, Month, Day, Hour, Min, Sec, ModelName]),
+  Data = create_table_term(ModelName, ColumnSpecs),
+  file:write_file(FileName,io_lib:fwrite("~p.\n",[Data]));
+generate(["migration" | [MigrationName | ColumnSpecs]]) ->
+  io:format("Migrating with ~s with columns ~p~n", [MigrationName, ColumnSpecs]).
 
 run_for(Dir) ->
-  Pid = spawn(fun() -> reke:generate() end),
+  Pid = spawn(fun() -> reke:execute() end),
   ensure_migration_table(Pid),
   case file:list_dir("db/migrate") of
     {ok, Files} ->
@@ -61,6 +70,7 @@ process_files([File | Rest], Pid, Dir) ->
   erlang:display(File),
   case file:consult("db/migrate/" ++ File) of
     {ok, Tokens} ->
+
       Term = proplists:get_value(Dir, Tokens),
       Pid ! Term,
       process_files(Rest, Pid, Dir);
@@ -91,5 +101,19 @@ column_ref({timestamps}) ->
 
 ensure_migration_table(Pid) ->
   Pid ! {create_table, "schema_migrations", [{string, "version"}]}.
+
+create_table_term(ModelName, _ColumnSpecs) ->
+  {up,
+    {create_table, ModelName,
+      [
+        {string, "name"},
+        {string, "git_account_name"},
+        {string, "code"},
+        {string, "access_token"},
+        {timestamps}
+      ]
+    }
+  }.
+
 
 %% Insert all version numbers in schema_migrations
