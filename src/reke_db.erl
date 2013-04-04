d19d6621de63930181f08101a77cc05bbc5bbb3a
Philp Feairheller <phil@hoopla.net>
2013-04-03 21:21:14 -0400

    Honoring migrations table for migrate command.  Inserting after success, only running those not run yet.

--- src/reke_db.erl
+++ src/reke_db.erl
@@ -11,13 +11,13 @@
 -export([init/1, handle_call/3, handle_cast/2]).
 
 start_link() ->
-  ets:new(migrations, [named_table, {}]),
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 stop() ->
   gen_server:cast(?MODULE, stop).
 
 init(_) ->
+  ets:new(migrations, [named_table, ordered_set]),
   case ensure_migration_table() of
     {ok, Migrations} ->
       {ok, Migrations};
@@ -52,10 +52,7 @@ handle_call({insert_schema_migration, Version}, _Pid, LoopData) ->
     Result ->
       erlang:display(Result)
   end,
-  {reply, Result, LoopData};
-handle_call({should_run, Version}, _Pid, LoopData) ->
-
-  {reply, true, LoopData}.
+  {reply, Result, LoopData}.
 
 handle_cast(stop, LoopData) ->
   {stop, normal, LoopData}.
@@ -64,8 +61,10 @@ migrate(_Options) ->
   start_link(),
   run_for(up).
 
-rollback(_Options) ->
+rollback(Options) ->
   start_link(),
+  io:format("Options ~p~n", [Options]),
+
   run_for(down).
 
 run_for(Dir) ->
@@ -78,22 +77,28 @@ run_for(Dir) ->
   end.
 
 process_files([], _Dir, Processed) ->
-  insert_schema_migrations(Processed);
-%%   stop();
+  insert_schema_migrations(Processed),
+  stop();
 process_files([File | Rest], Dir, Processed) ->
   [NumStr | _] = string:tokens(File, "_"),
-  erlang:display(File),
-  case file:consult("db/migrate/" ++ File) of
-    {ok, Tokens} ->
-      Term = proplists:get_value(Dir, Tokens),
-      case gen_server:call(?MODULE, Term) of
-        {ok, _Result} ->
-          process_files(Rest, Dir, [NumStr | Processed]);
-        {error, Error} ->
-          io:format("Error with ~s: ~p~n", [File, Error])
-      end;
-    _ ->
-      {error, unknown}
+  case ets:lookup(migrations, NumStr) of
+    [_] ->
+      io:format("Migration ~s already complete.~n", [NumStr]),
+      process_files(Rest, Dir, Processed);
+    [] ->
+      case file:consult("db/migrate/" ++ File) of
+        {ok, Tokens} ->
+          io:format("Running Migration ~s.~n", [NumStr]),
+          Term = proplists:get_value(Dir, Tokens),
+          case gen_server:call(?MODULE, Term) of
+            {ok, _Result} ->
+              process_files(Rest, Dir, [NumStr | Processed]);
+            {error, Error} ->
+              io:format("Error with ~s: ~p~n", [File, Error])
+          end;
+        _ ->
+          {error, unknown}
+      end
   end.
 
 generate_drop_statement(TableName) ->
@@ -122,6 +127,7 @@ ensure_migration_table() ->
   reke_connection:squery(reke, Stmt),
   case reke_connection:squery(reke, "SELECT version FROM schema_migrations ORDER BY version ASC") of
     {ok, _Columns, Rows} ->
+      ets:insert(migrations, lists:map(fun(E) -> {binary:bin_to_list(element(1, E))} end, Rows)),
       {ok, Rows};
     {error, Error} ->
       {error, Error}
