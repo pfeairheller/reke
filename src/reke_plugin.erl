30b71142c9ddd90b686039e3bf1bffd2054572d1
Philp Feairheller <phil@hoopla.net>
2013-04-07 13:42:20 -0400

    First bits of rebar commands.  Not liking their command-line capabilities, but just have to adjust my thinking a bit.  That happens a lot in erlang world.

---
+++ src/reke_plugin.erl
@@ -0,0 +1,39 @@
+%% Copyright
+-module(reke_plugin).
+-author("pfeairheller").
+
+-export([
+  'reke-db:migrate'/2,
+  'reke-db:rollback'/2
+]).
+
+'reke-db:migrate'(Config, _AppFile) ->
+  OrigPath = code:get_path(),
+  true = code:add_path(rebar_utils:ebin_dir()),
+
+  Result = case rebar_utils:processing_base_dir(Config) of
+    true ->
+      reke:start(),
+      reke:execute(["db", "migrate"]),
+      ok;
+    false -> ok
+  end,
+
+  true = code:set_path(OrigPath),
+  Result.
+
+
+'reke-db:rollback'(Config, _AppFile) ->
+  OrigPath = code:get_path(),
+  true = code:add_path(rebar_utils:ebin_dir()),
+
+  Result = case rebar_utils:processing_base_dir(Config) of
+    true ->
+      reke:start(),
+      reke:execute(["db", "rollback"]),
+      ok;
+    false -> ok
+  end,
+
+  true = code:set_path(OrigPath),
+  Result.
